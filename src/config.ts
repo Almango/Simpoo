020401113bfd6c0c68f0a404a964cba57c5acb23
Almango <sliveralmango@outlook.com>
2026-04-19 14:44:47 +0800

    '1.1.0'

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,10 @@ export const BASE_CONFIG = {
   site_url: 'https://blog.almango.cn',
   create_time: '2024-01-23',
 }
+export const POST_CONFIG = {
+  page_size: 8,     // 每页显示数量
+}
 
 export const DEVELOPMENT = {
-  VERSION: '1.0.0',
+  VERSION: '1.1.0',
 }
